65cb5c633851da1c0e7995b90fd11de927f96350
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-09 13:54:27 +0530

    new app useing hooks

--- src/App.js
+++ src/App.js
@@ -34,25 +34,61 @@
 //below is function components and above is class components
 //.........................................................................
 
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 
 // const App=()=>
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+
+//   useEffect(()=>{
+//     document.title= `Clicked ${count} times`;
+//   })
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Counter App</h1>
+//       <button class="btn btn-light" onClick={increment}>
+//         Clicked {count} times
+//       </button>
+//     </div>
+//   );
+// }
+
+const App = ()=>{
+
+  const [news,setNews]= useState([]);
+
+  // fetch news
+
+  const fetchNews =()=>{
+
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    .then (result => result.json())
+    // .then(data => console.log(data));
+    .then (data => setNews(data.hits))
+    .catch (error => console.log(error))
+  }
+
+  useEffect(()=>{
+    fetchNews();
+
+
+  })
 
   return (
     <div>
-      <h1>Counter App</h1>
-      <button class="btn btn-light" onClick={increment}>
-        Clicked {count} times
-      </button>
-    </div>
-  );
+    <h2>News</h2>
+    {news.map((n, i) => (
+      <p key={i}>{n.title}</p>
+    ))}
+  </div>
+);
 }
 
 export default App;
